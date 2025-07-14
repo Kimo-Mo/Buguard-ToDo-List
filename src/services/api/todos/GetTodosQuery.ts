55fa59df107944f27b42b6cda9822197d5cf2a8b
kareem mohamed <kimomo844@gmail.com>
2025-07-14 19:06:07 +0300

    add API client and CRUD operations for todos

---
+++ src/services/api/todos/GetTodosQuery.ts
@@ -0,0 +1,11 @@
+import { useQuery } from "@tanstack/react-query";
+import { getTodos } from "./todos.api";
+
+export const GET_TODOS_QUERY_KEY = "/todos";
+
+export const useGetTodosQuery = () => {
+  return useQuery({
+    queryKey: [GET_TODOS_QUERY_KEY  ],
+    queryFn: getTodos,
+  });
+};
---
+++ src/services/api/todos/todos.api.ts
@@ -0,0 +1,23 @@
+import type { ITodo } from "@/services/types/Todo";
+import { ApiClient } from "../ClientApi";
+
+export const getTodos = async () => {
+  const res = await ApiClient.get("/todos");
+  return res.data;
+};
+
+
+export const createTodo = async (todo: ITodo) => {
+  const res = await ApiClient.post("/todos", todo);
+  return res.data;
+};
+
+export const updateTodo = async (todo: Partial<ITodo>, id: string) => {
+  const res = await ApiClient.put(`/todos/${id}`, todo);
+  return res.data;
+};
+
+export const deleteTodo = async (id: string) => {
+  const res = await ApiClient.delete(`/todos/${id}`);
+  return res.data;
+};
