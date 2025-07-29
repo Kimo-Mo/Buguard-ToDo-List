d4b271e7e045f9bfac94388bfe363f636e193dd5
kareem mohamed <kimomo844@gmail.com>
2025-07-29 14:03:09 +0300

    feat(ui): add ErrorNotFound component and update exports

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,2 +1,4 @@
-export * from './SegmentedTabs';
+export { default as SegmentedTabs } from './SegmentedTabs';
 export { default as TaskModal } from './TaskModal';
+export { default as LoadingComponent } from './LoadingComponent';
+export { default as ErrorNotFound } from './ErrorNotFound';
