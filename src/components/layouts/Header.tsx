f8bf9887635922724ac36b9341baac371a91cd72
kareem mohamed <kimomo844@gmail.com>
2025-08-03 17:23:24 +0300

    refactor(Header): replace assignee selection with auth0 user info
    Remove assignee-related logic and replace with Auth0 authentication flow
    Simplify header component by removing unused imports and state management

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -1,8 +1,7 @@
-import { Avatar, Input, Select, type SelectProps } from 'antd';
+import { Avatar, Input } from 'antd';
 import { BellOutlined, UserOutlined } from '@ant-design/icons';
-import type { IAssignee } from '@/services/types';
-import { useGetAssigneesQuery } from '@/services/api';
-import { useMemo, useState } from 'react';
+
+import { useAuth0 } from '@auth0/auth0-react';
 const { Search } = Input;
 
 interface HeaderProps {
@@ -10,20 +9,8 @@ interface HeaderProps {
   setSearchQuery: (value: string) => void;
 }
 const Header = ({ searchQuery, setSearchQuery }: HeaderProps) => {
-  const { data: assignees } = useGetAssigneesQuery();
-  const [assigneeAvatar, setAssigneeAvatar] = useState<string>('');
-  const assigneeOptions: SelectProps['options'] = useMemo(() => {
-    return assignees?.map((assignee: IAssignee) => ({
-      value: assignee.id,
-      label: <span>{assignee.name}</span>,
-    }));
-  }, [assignees]);
-  const handleSelectChange = (value: string) => {
-    const assignee = assignees?.find(
-      (assignee: IAssignee) => assignee.id === value
-    );
-    setAssigneeAvatar(assignee?.avatar || '');
-  };
+  const { user, isAuthenticated } = useAuth0();
+
   return (
     <nav className="flex flex-col-reverse md:flex-row justify-between items-center gap-4 bg-card py-3 px-6 m-6 shadow-xs rounded-[37px] ">
       <Search
@@ -37,25 +24,15 @@ const Header = ({ searchQuery, setSearchQuery }: HeaderProps) => {
       <div className="flex gap-4 flex-col md:flex-row justify-center items-center">
         <div className="flex gap-4">
           <BellOutlined className="*:w-6 *:h-6" />
-          {assigneeAvatar.length === 0 ? (
-            <Avatar icon={<UserOutlined />} />
+          {isAuthenticated ? (
+            <Avatar src={user?.picture} />
           ) : (
-            <Avatar
-              src={assigneeAvatar}
-              className="!bg-description/50"
-            />
+            <Avatar icon={<UserOutlined />} />
           )}
         </div>
         <div className="flex items-center md:items-start flex-row md:flex-col gap-2 md:gap-0">
           <p>Workspace</p>
-          <Select
-            variant="borderless"
-            placeholder="Select assignee"
-            options={assigneeOptions}
-            style={{ width: 120 }}
-            className="*:!p-0"
-            onChange={(value) => handleSelectChange(value)}
-          />
+          <p>{isAuthenticated ? user?.name : 'username'}</p>
         </div>
       </div>
     </nav>
