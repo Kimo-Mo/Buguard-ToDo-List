2dfbba8a687a179448d6dc072accb4354cd3dc34
kareem mohamed <kimomo844@gmail.com>
2025-08-03 14:25:54 +0300

    style(NotFound): change button type from primary to default

--- src/components/NotFound/NotFound.tsx
+++ src/components/NotFound/NotFound.tsx
@@ -9,7 +9,7 @@ const NotFound = () => {
       title="404"
       subTitle="Sorry, the page you visited does not exist."
       extra={
-        <Button type="primary" onClick={() => navigate('/')}>
+        <Button type="default" onClick={() => navigate('/')}>
           Back Home
         </Button>
       }
