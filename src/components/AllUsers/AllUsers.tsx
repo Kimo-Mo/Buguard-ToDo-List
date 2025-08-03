61a829ea4e28a8fdc288cc691c9404045ac1a330
kareem mohamed <kimomo844@gmail.com>
2025-08-03 17:22:27 +0300

    refactor(AllUsers): improve error and loading state handling
    Move error and loading state checks inside the return statement for better component structure and readability

--- src/components/AllUsers/AllUsers.tsx
+++ src/components/AllUsers/AllUsers.tsx
@@ -5,24 +5,29 @@ import { ErrorNotFound, LoadingComponent } from '../ui';
 
 const AllUsers = () => {
   const { data: assignees, error, isLoading } = useGetAssigneesQuery();
-  if (error) return <ErrorNotFound />;
-  if (isLoading) return <LoadingComponent />;
+
   return (
     <div>
       <h1 className="text-lg shadow-sm rounded-md bg-card px-6 py-3">
         All Users
       </h1>
-
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4 mt-4">
-        {assignees.map((user: IAssignee) => (
-          <div
-            key={user.id}
-            className="flex flex-col items-center gap-y-3 bg-card rounded-md shadow-sm p-4">
-            <Avatar src={user.avatar} className="!size-8 !bg-description/50" />
-            <p>{user.name}</p>
-          </div>
-        ))}
-      </div>
+      {error && <ErrorNotFound />}
+      {!error && isLoading && <LoadingComponent />}
+      {!error && !isLoading && (
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4 mt-4">
+          {assignees.map((user: IAssignee) => (
+            <div
+              key={user.id}
+              className="flex flex-col items-center gap-y-3 bg-card rounded-md shadow-sm p-4">
+              <Avatar
+                src={user.avatar}
+                className="!size-8 !bg-description/50"
+              />
+              <p>{user.name}</p>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
